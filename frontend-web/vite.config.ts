1f6570d8bad2fd4f8f994c2f66b31140e965339d
sinan <sinan.bouhoutani@edu.uah.es>
2026-06-06 17:53:20 +0200

    refactor: centralizar y parametrizar variables de entorno en el frontend

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -6,6 +6,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  envDir: '../',
+  envPrefix: ['VITE_', 'GOOGLE_CLIENT_ID'],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
